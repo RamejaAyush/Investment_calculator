5b5d2583063f6a0ea6eaede07701578bacf23922
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-08-24 15:05:10 +0530

    basic calculator functions are added

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 // all elements
-const calToggleSwitch = document.querySelector('#cal-switch')
+let calToggleSwitch = document.querySelector('#cal-switch')
 
-const curToggleSwitch = document.querySelector('#cur-switch')
+let curToggleSwitch = document.querySelector('#cur-switch')
 
-const status = document.querySelector('#status')
+let status = document.querySelector('#status')
+
+let investmentText = document.querySelector('#investment-text')
+
+let Investment = document.querySelector('#Investment')
+
+let numberOfYears = document.querySelector("#numberOfYears")
+
+let expectedRate = document.querySelector("#expected")
+
+let Result = document.querySelector(".calculate")
+
+let totalInvest = document.querySelector('#total')
+
+let amountGained = document.querySelector('#Gained')
+
+let Maturity = document.querySelector("#maturity")
+
+// functions
+function lumpsum(){
+    Maturity.value = Math.round(Math.pow((1 + expectedRate.value / 100), numberOfYears.value) * Investment.value)
+}
+
+function sip(){
+    Maturity.value = Math.round((Math.pow((1 + (Math.pow((1 + expectedRate.value / 100), (1 / 12)) - 1)), (numberOfYears.value * 12)) - 1) / (Math.pow((1 + expectedRate.value / 100), (1 / 12)) - 1) * Investment.value)
+}
+
+Result.setAttribute("onclick", "sip()")
 
 // event listeners
 calToggleSwitch.addEventListener("click", 
 (event) => {
     if(event.target.checked){
         status.innerHTML = 'Lumpsum Calculator'
+        investmentText.innerText = 'Total Investment'
+        Result.setAttribute("onclick", "lumpsum()")
     }else{
         status.innerHTML = 'SIP Calculator'
+        investmentText.innerText = 'Monthly Investment'
+        Result.setAttribute("onclick", "sip()")
     }
 })
 
