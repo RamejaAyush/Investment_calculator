30386c3147201dfa5c678c9b31a6823abf45d0c0
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-08-24 14:16:47 +0530

    added javaScript

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("working")
+// all elements
+const calToggleSwitch = document.querySelector('#cal-switch')
+
+const curToggleSwitch = document.querySelector('#cur-switch')
+
+const status = document.querySelector('#status')
+
+// event listeners
+calToggleSwitch.addEventListener("click", 
+(event) => {
+    if(event.target.checked){
+        status.innerHTML = 'Lumpsum Calculator'
+    }else{
+        status.innerHTML = 'SIP Calculator'
+    }
+})
+
+curToggleSwitch.addEventListener("click", 
+(event) => {
+    if(event.target.checked){
+        console.log("currency switch is active")
+    }else{
+        console.log("currency switch is not active")
+    }
+})
